b0332584281fb9ff41387beb6926825c61b0ad1a
Khaledwaled10 <khaledwaled8713@gmail.com>
2025-08-10 22:10:36 +0300

    Replace react-loader-spinner with react-spinners

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,25 +1,10 @@
-import React from 'react'
-import {RotatingLines  } from 'react-loader-spinner'
+import React from "react";
+import { ClipLoader } from "react-spinners";
+
 export default function Loading() {
   return (
-    < >
-    <div className=' flex justify-center items-center'>
-
-      <RotatingLines 
-  visible={true}
-  height="96"
-  width="60"
-  color=""
-  strokeColor='oklch(54.6% 0.245 262.881)'
-  strokeWidth="5"
-  animationDuration="0.75"
-  ariaLabel="rotating-lines-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  />
-  </div>
-
-    
-    </>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <ClipLoader color="#155dfb" size={50} />
+    </div>
+  );
 }
